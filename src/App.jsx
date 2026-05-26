c5ccaa71c74be8db1a3f875ee484821cc10f0141
k-chahuan <k.e.chahuan@gmail.com>
2026-05-26 02:36:15 -0400

    Creacion de docs y componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Marco from './components/Marco.jsx'
 
 function App() {
-return <h1 className="text-3xl font-bold text-blue-600">Funciona</h1>
+  return <Marco />
 }
+
 export default App
